dfbae84a4c42d7dbaa9e7c67e4bb93e8a4efe0bd
Frederik Pigeon <fredprivate@duck.com>
2024-11-08 14:37:43 -0500

    test

--- Assets/Scripts/Floater.cs
+++ Assets/Scripts/Floater.cs
@@ -23,6 +23,7 @@ public class Floater : MonoBehaviour
         objectWithFloaters.AddForceAtPosition(Physics.gravity / floaters, transform.position, ForceMode.Acceleration);
 
         Search.startPositionWS = transform.position;
+        Search.includeDeformation = true;
         waterSurface.ProjectPointOnWaterSurface(Search, out SearchResult);
 
         // Si le kayak est sous la surface de l'eau
